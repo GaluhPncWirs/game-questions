978da9a07f1e9f39226c2c5fe8ae37cbf3f895a7
galuh panca wirasa <Galuh panca wirasa>
2025-05-15 11:43:15 +0700

    meembuat halaman pertanyyaan

--- src/app/mainContent/questions/page.tsx
+++ src/app/mainContent/questions/page.tsx
@@ -6,13 +6,30 @@ export default function ChoseenLevel() {
     <div className="bg-slate-900 h-screen">
       <div className="w-11/12 mx-auto">
         <div className="w-1/5 pt-5 mx-auto">
-          <h1 className="text-white mb-10 text-center bg-slate-600 py-3 text-2xl font-bold rounded-xl">
+          <h1 className="text-white text-center bg-slate-600 py-3 text-2xl font-bold rounded-xl">
             00:10
           </h1>
         </div>
-        <div className="w-3/5 bg-slate-700 h-96 mx-auto rounded-xl">
-          {/* <img src="" alt="" /> */}
+        <div className="w-3/5 bg-slate-700 h-96 mx-auto rounded-xl my-9">
+          <img src="" alt="gambar pertannyaan" />
+          <h1 className="font-semibold text-2xl text-slate-200 text-center">
+            siapakah nama tokoh ini
+          </h1>
         </div>
+        <ul className="text-white flex justify-evenly">
+          <li className="bg-slate-600 cursor-pointer py-6 px-10 rounded-xl hover:bg-slate-700">
+            lorem lorem
+          </li>
+          <li className="bg-slate-600 cursor-pointer py-6 px-10 rounded-xl hover:bg-slate-700">
+            ipsum ipsum
+          </li>
+          <li className="bg-slate-600 cursor-pointer py-6 px-10 rounded-xl hover:bg-slate-700">
+            dolor dolor
+          </li>
+          <li className="bg-slate-600 cursor-pointer py-6 px-10 rounded-xl hover:bg-slate-700">
+            amet amet
+          </li>
+        </ul>
       </div>
     </div>
   );
